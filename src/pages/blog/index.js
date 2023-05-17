7d528f51222f39c35566f7194dc685bdcbe0e0ad
AurelioMiranda <80250266+AurelioMiranda@users.noreply.github.com>
2023-05-17 14:53:24 +0100

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -3,6 +3,25 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
+        ))
+      }
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query {
@@ -14,30 +33,11 @@ export const query = graphql`
           slug
         }
         id
+        excerpt
       }
     }
   }
 `
-
-const BlogPage = ({ data }) => {
-  return (
-    <Layout pageTitle="My Blog Posts">
-      {
-        data.allMdx.nodes.map(node => (
-          <article key={node.id}>
-            <h2>
-            {console.log(node.frontmatter)}
-              <Link to={`/blog/${node.frontmatter.title.toLowerCase().replaceAll(' ', '-')}`}>
-                {node.frontmatter.title}
-              </Link>
-            </h2>
-            <p>Posted: {node.frontmatter.date}</p>
-          </article>
-        ))
-      }
-    </Layout>
-  )
-}
 
 export const Head = () => <Seo title="My Blog Posts" />
 
